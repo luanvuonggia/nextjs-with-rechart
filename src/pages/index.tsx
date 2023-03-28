47fc2af649e64f1dd4956e3a00fe824943271c43
Luan Vuong <luanvuong@orgos.net>
2023-03-28 17:19:28 +0700

    init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router';
+
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  const router = useRouter();
+
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Next.js Boilerplate Presentation"
+          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+        />
+      }
+    >
+      <a href="https://github.com/ixartz/Next-js-Boilerplate">
+        <img
+          src={`${router.basePath}/assets/images/nextjs-starter-banner.png`}
+          alt="Nextjs starter banner"
+        />
+      </a>
+    </Main>
+  );
+};
+
+export default Index;
